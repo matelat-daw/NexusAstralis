8198c3c1b7bfaeed7b2ff9a14d2dd98fae072e92
Orion68 <orions68@gmail.com>
2025-04-27 19:19:08 +0100

    Update 27-4-2025 19:19

---
+++ Nexus-Front/src/app/rest/rest.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+@Component({
+  selector: 'app-rest',
+  templateUrl: './rest.component.html',
+  styleUrls: ['./rest.component.css']
+})
+export class RestComponent {
+
+  posts :any
+  constructor(http :HttpClient) {
+    //  let response = http.get('https://jsonplaceholder.typicode.com/posts');
+     let response = http.get('https://localhost:7035/api/Account/Users');
+     response.subscribe((data)=>this.posts=data);
+  }
+}
