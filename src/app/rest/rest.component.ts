3955c2a1cdca7c2a2312693ff59c7d0768f7046c
Orion68 <orions68@gmail.com>
2025-04-27 20:48:49 +0100

    Update 27-4-2025 20:48

--- src/app/rest/rest.component.ts
+++ src/app/rest/rest.component.ts
@@ -11,7 +11,8 @@ export class RestComponent {
   posts :any
   constructor(http :HttpClient) {
     //  let response = http.get('https://jsonplaceholder.typicode.com/posts');
-     let response = http.get('https://88.24.29.240/api/Account/Users');
+    //  let response = http.get('https://88.24.29.240/api/Account/Users');
+    let response = http.get('https://localhost:4200/api/Account/Users')
      response.subscribe((data)=>this.posts=data);
   }
 }
